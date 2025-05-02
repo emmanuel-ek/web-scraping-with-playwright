24793285379f443b4624ab380d06b1b6d202dc47
MOSFET <kwizeraemmanuel11@gmail.com>
2025-05-02 19:33:43 +0530

    creating a page crawling bot

--- automated_login.py
+++ automated_login.py
@@ -3,12 +3,27 @@ import time
 
 class WebHackingBot:
     def __init__(self):
-        with sync_playwright() as p:
-            self.browser = p.chromium.launch(headless=False)
-            self.context = self.browser.new_context()
-            self.page = self.context.new_page()
+        self.pw = sync_playwright().start()  
+        self.browser = self.pw.chromium.launch(headless=False)
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
+        
+        #login credentials
+        self.username = "student"
+        self.password = "Password123"
     
-    def handle_automated_login(self):
+    def crawl_on_contact_page(self):
+        contact_page_url = self.page.locator('a', has_text="Contact").get_attribute("href")
+        if contact_page_url.count() <= 0:
+            print("contact page not found")
+            return  
+        
+        # This function will handle custom page filling
+        # by passing some captcha
+        # and submittig the form
+        print(f"Contact Page URL: {contact_page_url}")        
+    
+    def handle_automated_login(self) -> bool:
         self.page.goto("https://practicetestautomation.com/practice-test-login/")
         
         # check if the correct page has been returned
@@ -17,8 +32,7 @@ class WebHackingBot:
         
         if count is None or count <= 0:
             print("wrong page content was returned")
-            return
-        
+            return False
         
         # check if the page has login field
         username_field = self.page.locator("input#username")
@@ -26,11 +40,11 @@ class WebHackingBot:
         
         if username_field.count() <= 0 or username_field.count() <= 0:
             print("username or password field is not found")
-            return
+            return False
         
         # fillin the login form
-        username_field.fill("student")
-        password_field.fill("Password1234")
+        username_field.fill(self.username)
+        password_field.fill(self.password)
         
         submit_button = self.page.locator("button#submit")
         submit_button.click()
@@ -39,13 +53,20 @@ class WebHackingBot:
         try:
             self.page.locator("h1.post-title", has_text="Logged In Successfully").wait_for(timeout=3000)
             print("Login successful.")
+            return True
         except:
             print("Login failed. Try using different credentials.")
-            return
-
+            return False
         
-        self.browser.close()
         
 if __name__ == "__main__":
     bot = WebHackingBot()
-    bot.handle_automated_login()
+    loggin_status = bot.handle_automated_login()
+    
+    # crawl on the contact page 
+    # and fill in contact form
+    if loggin_status:
+        bot.crawl_on_contact_page()
+    
+    # Close the browser at the end
+    bot.browser.close()
